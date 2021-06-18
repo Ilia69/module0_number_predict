8b4b5c896dea919ad760a057d272ad1b3b609d96
ilya.kuptcov <ilya.kuptcov@yandex.ru>
2021-06-18 18:53:13 +0300

    Recursive binary search was added and tested. ReadMe was updated

--- game_core.py
+++ game_core.py
@@ -14,12 +14,13 @@ def game_core_v1(number):
 
 
 def game_core_v2(number):
-    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем его в зависимости от того, больше оно или меньше нужного.
-       Функция принимает загаданное число и возвращает число попыток'''
+    '''Сначала устанавливаем любое random число, а потом уменьшаем или увеличиваем
+        его в зависимости от того, больше оно или меньше искомого.
+        Функция принимает загаданное число и возвращает число попыток'''
     count = 1
-    predict = np.random.randint(1,101)
+    predict = np.random.randint(1, 101)
     while number != predict:
-        count+=1
+        count += 1
         if number > predict:
             predict += 1
         elif number < predict:
@@ -32,5 +33,14 @@ def game_core_v3(number):
     left_edge = 1
     right_edge = 100
     count = search_alg.binary_search(left_edge, right_edge, number)
+
+    return count
+
+
+def game_core_v4(number):
+    '''Запускаем рекурсивный двоичный поиск на отрезке [1 , 100]'''
+    left_edge = 1
+    right_edge = 100
+    count = search_alg.recursive_binary_search(left_edge, right_edge, number)
 
     return count
--- search_alg.py
+++ search_alg.py
@@ -18,3 +18,22 @@ def binary_search(left_edge, right_edge, number):
             right = predict
             predict -= round((right - left) / 2)
     return count # Выход из цикла, если угадали
+
+
+def recursive_binary_search(left_ed, right_ed, number, count=0):
+    '''Реализует подсчёт числа шагов при поиске number в отрезке
+        [left_edge, right_edge] при помощи рекурсивного двоичного поиска'''
+    left = left_ed
+    right = right_ed
+    predict = left + round((right - left) / 2)
+    count += 1
+
+    if (number == left) or (number == right) or (number == predict):
+        return count
+
+    if number < predict:
+        right = predict
+        return recursive_binary_search(left, right, number, count)
+    elif number > predict:
+        left = predict
+        return recursive_binary_search(left, right, number, count)
